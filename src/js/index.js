6e8e56c0b07c25a9d17ebc22bd8455a31a41782b
Qidia <qidian_n@mail.ru>
2024-10-01 01:02:13 +0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,10 @@
 import { generateGame } from './game.js';
 import { handleClick } from './eventHandlers.js';
 import { imageInParts } from './canvas.js';
+import backgroundCard from './src/img/background_card.jpg';
 
 document.addEventListener('DOMContentLoaded', () => {
-  imageInParts('./src/img/background_сard.jpg'); // Делим изображение на части
+  imageInParts(backgroundCard); // Делим изображение на части
   generateGame(); // Генерация поля игры
   document.addEventListener('click', handleClick); // Прикрепление обработчиков событий
 });
